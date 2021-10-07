abc9b60c0b767642bf78faceb54df208eb54aef7
Alexandre Trigueros <80602661+trigueros-tech@users.noreply.github.com>
2021-10-08 00:29:05 +0200

    Initial commit

---
+++ backend/Api/Api/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var claim = User.FindFirst("preferred_username");
+            return $"Hello {claim?.Value}";
+        }
+    }
+}
